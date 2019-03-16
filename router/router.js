00bba2f4488c2efc601508f514e0afacaac704b0
denteyon <htetmyataung2027@gmail.com>
2019-03-16 19:36:16 -0400

    add lat and long

--- router/router.js
+++ router/router.js
@@ -1,21 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const User = require('../models/user');
+const request = require('request');
 module.exports.router = express.Router();
 
 module.exports.router.post("/signup", bodyParser.urlencoded({ extended: false }), (req, res) => {
-	if (req.body.name && req.body.password && req.body.phone && req.body.blood_type && req.body.longitude && req.body.latitude) {
-		const user = new User({
-			name: req.body.name,
-			password: req.body.password,
-			bloodType: req.body.blood_type,
-			latitude: req.body.longitude,
-			longitude: req.body.latitude,
-		});
-		user.save(e => {
-			if (e) console.log(e);
-		});
-		res.redirect("/map.html");
+    var latitude = 0, longitude = 0;
+	if (req.body.name && req.body.password && req.body.phone && req.body.blood_type && req.body.address) {
+        var geoURL = `https://geocoder.api.here.com/6.2/geocode.json?app_id=xgJpkwVEfD6Irqxucqy2&app_code=TeJKeLplY0w4FE5zrDKzlA&searchtext=${req.body.address}`
+        request(geoURL, (error, response, body)=>{
+            if (error) console.log(error);
+            try {
+                var geoJSON = JSON.parse(body);
+                latitude = geoJSON.Response.View[0].Result[0].Location.DisplayPosition.Latitude;
+                longitude = geoJSON.Response.View[0].Result[0].Location.DisplayPosition.Longitude;
+            } catch (e) {  
+            }
+            const user = new User({
+                name: req.body.name,
+                password: req.body.password,
+                bloodType: req.body.blood_type,
+                latitude: latitude,
+                longitude: longitude,
+            });
+            user.save(e => {
+                if (e) console.log(e);
+            });
+            res.redirect("/map.html");
+        });
 	}
 	else {
 		console.log(req.body);

8628218c81df9f587addbdfb01154f894aa13cf8
Akash Mozumdar <akashmozumdar@gmail.com>
2019-03-16 18:20:00 -0400

    bugfix

--- app.js
+++ app.js
@@ -15,8 +15,7 @@ const port = 3000;
 const router = require('./router/router');
 
 // Set up mongoose connection
-var mongoose = require('mongoose');
-var dev_db_url = 'mongodb+srv://ashaygyi:Ashaygyi@full-cycle-rg79z.gcp.mongodb.net/test?retryWrites=true';
+var dev_db_url = 'mongodb+srv://ashaygyi:ashaygyi@full-cycle-rg79z.gcp.mongodb.net/fullcycleApp?retryWrites=true';
 var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;

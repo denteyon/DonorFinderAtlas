3333e9025c2cab701a1d1d791ccb1976fe52391d
Akash Mozumdar <akashmozumdar@gmail.com>
2019-03-16 17:41:47 -0400

    add signup api

--- router/router.js
+++ router/router.js
@@ -1,3 +1,9 @@
 const express = require('express');
-const router = express.Router();
+const bodyParser = require('body-parser');
+module.exports.router = express.Router();
 
+module.exports.router.post("/signup", bodyParser.urlencoded({ extended: false }), (req, res) => {
+	console.log(req.body.name, req.body.password, req.body.phone);
+	res.send("Thanks for signing up!");
+	res.end();
+});

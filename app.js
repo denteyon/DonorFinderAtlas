b5a6fb34034d03816a2d56efa897d25f014c379a
denteyon <htetmyataung2027@gmail.com>
2019-03-16 15:51:32 -0400

    add database integration

--- app.js
+++ app.js
@@ -5,11 +5,21 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
 const helmet = require('helmet');
+const mongoose = require('mongoose');
 
 const publicPath = path.join(__dirname+'public');
 
 var app = express();
 const port = 3000;
+
+// Set up mongoose connection
+var mongoose = require('mongoose');
+var dev_db_url = '<mongoatlas>';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
